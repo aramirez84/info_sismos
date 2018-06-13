b936f29f7bdd6cccfadb4151d7f55b6e241df72f
aramirez84 <alexro2404@hotmail.com>
2018-06-13 16:35:53 -0500

    Agregando archivos header y footer al proyecto

---
+++ application/views/footer.php
@@ -0,0 +1,6 @@
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="<?php echo base_url()?>bootstrap/js/jquery-3.3.1.min.js" type="text/javascript"></script>
+    <script src="<?php echo base_url()?>bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+  </body>
+</html>
---
+++ application/views/header.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="es">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="<?php echo base_url()?>bootstrap/css/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="<?php echo base_url()?>bootstrap/css/bootstrap-grid.min.css" type="text/css">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
