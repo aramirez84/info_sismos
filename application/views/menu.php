0060731bd2153059cc3453c4fccdd9c4e2dfba08
aramirez84 <alexro2404@hotmail.com>
2018-06-19 17:04:54 -0500

    Agregando menu de la aplicaicion

---
+++ application/views/menu.php
@@ -0,0 +1,20 @@
+<nav class="navbar navbar-expand-md bg-dark navbar-dark">
+    <form class="form-inline" action="/action_page.php">
+    <input class="form-control mr-sm-2" type="text" placeholder="Search">
+    <button class="btn btn-success" type="submit">Search</button>
+  </form>
+  <div class="collapse navbar-collapse" id="collapsibleNavbar">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>    
+    </ul>
+  </div>  
+</nav>
+<br>
