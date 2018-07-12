84ee5f55d27d631393509e9b518ded980f54c541
alejandro <alexro2404@hotmail.com>
2018-07-12 01:08:38 -0500

    Creando formulario para cargar archivo csv

---
+++ application/views/subir_archivo.php
@@ -0,0 +1,20 @@
+<div class="head-body-primary2">
+    <div class="title-app-head">
+        <h3>Módulo para cargar informaci&oacute;n</h3>
+    </div>
+</div>
+<div class="head-body-libros">
+    <div class="box-books-load">
+        <table>
+            <tr>
+                <td style="width: 50px"></td>
+		<div class="btn">
+                    <i class="fa  fa-cloud-upload">
+                        <input id="portada" name="portada" class="input-file" type="file" required="">
+                    </i>
+                </div>
+		</td>
+            </tr>
+        </table>
+    </div>
+</div>
