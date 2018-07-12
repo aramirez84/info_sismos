670e5c2d97427c4053c706bfdc8c0df79530287f
alejandro <alexro2404@hotmail.com>
2018-07-12 01:06:40 -0500

    Generando metodos para obtener información de las tablas en la base de datos

--- application/models/consultas.php
+++ application/models/consultas.php
@@ -12,8 +12,54 @@ class Consultas extends CI_Model{
     
     function get_zona()
     {
-        $query=  $this->db->get('zona');
-        return $query->result();
+        $queryZonas=  $this->db->get('zona');
+        if($queryZonas->num_rows()!=0)
+         {
+            foreach($queryZonas->result_array() as $row)
+            {
+                $nombreZonas[]=$row;                
+            }
+            return $nombreZonas;
+         }
+    }
+    
+    function get_deleg_mun()
+    {
+        $queryDelegacion = $this->db->get('deleg_mun');
+        if($queryDelegacion->num_rows()!=0)
+         {
+            foreach($queryDelegacion->result_array() as $row)
+            {
+                $Delegacion[]=$row;                
+            }
+            return $Delegacion;
+         }
+    }
+    
+    function get_tipo_dano()
+    {
+        $queryTipo_dano=  $this->db->get('tipo_dano');
+        if($queryTipo_dano->num_rows()!=0)
+         {
+            foreach($queryTipo_dano->result_array() as $row)
+            {
+                $TipoDano[]=$row;                
+            }
+            return $TipoDano;
+         }
+    }
+    
+    function get_nivel_dano()
+    {
+        $queryNivel_dano=  $this->db->get('nivel_dano');
+        if($queryNivel_dano->num_rows()!=0)
+         {
+            foreach($queryNivel_dano->result_array() as $row)
+            {
+                $NivelDano[]=$row;                
+            }
+            return $NivelDano;
+         }
     }
 }
 /* End of file consultas.php */
