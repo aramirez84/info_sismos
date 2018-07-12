4ed2a0aaa7d610639f00c79a4fb055e57ec00d71
alejandro <alexro2404@hotmail.com>
2018-07-12 01:05:31 -0500

    Cargando información de la base de datos a las vistas

--- application/controllers/principal.php
+++ application/controllers/principal.php
@@ -9,11 +9,22 @@ class Principal extends CI_Controller {
     public function index()
     {
         $this->load->view('header');
-        $data['zonas']=  $this->Consultas->get_zona();
+        $data['zonas'] =  $this->Consultas->get_zona();
+        $data['delegacion'] = $this->Consultas->get_deleg_mun();
+        $data['nivelDano'] = $this->Consultas->get_nivel_dano();
+        $data['tipoDano'] = $this->Consultas->get_tipo_dano();
         $this->load->view('menu');
         $this->load->view('view_home',$data);
         $this->load->view('footer');
-    }   
+    }
+    
+    public function carga_info()
+    {
+        $this->load->view('header');
+        $this->load->view('menu');
+        $this->load->view('subir_archivo');
+        $this->load->view('footer');
+    }
 }
 
 /* End of file welcome.php */
