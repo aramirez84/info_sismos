9dcb7f35577070a0c55664701956f66640b953df
alejandro <alexro2404@hotmail.com>
2018-07-15 23:40:04 -0500

    Creand funciones para cargar y procesar el archivo csv en el sistema

--- application/controllers/principal.php
+++ application/controllers/principal.php
@@ -25,6 +25,63 @@ class Principal extends CI_Controller {
         $this->load->view('subir_archivo');
         $this->load->view('footer');
     }
+    
+    public function procesar_archivo()
+    {
+        if(isset($_FILES) && !empty($_FILES))
+        {
+            $nombreArchivo = $_FILES['sel_file']['name'];
+            $extArchivo = explode(".",$nombreArchivo);
+            if(strtolower(end($extArchivo)) == "csv")
+            {
+                $datos=$this->validaDatos($_FILES);
+                $insertar=$this->Consultas->insertarVivienda($datos);
+                if($insertar!=0)
+                {
+                    $data['mensaje']="Registro(s), agregado(s) correctamente";
+                }
+                else
+                {
+                    $data['mensaje']="Ocurrio un error favor de intentar de nuevo";
+                }
+            }
+            else
+            {
+                $data['mensaje']="Extención de archivo no válida";
+            }            
+        }
+        else
+        {
+            $data['mensaje']="Error al cargar el archivo, intentelo de nuevo";
+        }
+        //$data['mensaje']="";
+        $this->load->view('header');
+        $this->load->view('menu');
+        $this->load->view('subir_archivo',$data);
+        $this->load->view('footer');
+    }
+    
+    public function validaDatos($file)
+    {
+        $header = NULL;
+        $data = array();
+        $archivo = $file['sel_file']['tmp_name'];
+        $handle = fopen($archivo, "r");
+        while (($datos = fgetcsv($handle, 1000, ",")) !== FALSE)
+        {
+            if(!$header)
+            {
+                $header = $datos;
+            }
+            else
+            {
+                $data[] = array_combine($header, $datos);
+            }
+        }
+        fclose($handle);
+        return $data;        
+    }
+    
 }
 
 /* End of file welcome.php */
