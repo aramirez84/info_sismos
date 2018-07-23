35c4172f0f2af8cab85da6f1f50f3980726135c3
aramirez84 <alexro2404@hotmail.com>
2018-07-23 17:33:03 -0500

    Agregando diseño responsivo al menu

--- application/views/menu.php
+++ application/views/menu.php
@@ -1,20 +1,25 @@
-<nav class="navbar navbar-expand-md bg-dark navbar-dark">
-    <form class="form-inline" action="/action_page.php">
-    <input class="form-control mr-sm-2" type="text" placeholder="Buscar">
-    <button class="btn btn-success" type="submit">Buscar</button>
-  </form>
-  <div class="collapse navbar-collapse" id="collapsibleNavbar">
-    <ul class="navbar-nav">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="<?php echo base_url()?>">Menu</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+      <li class="nav-item active">
+        <a class="nav-link" href="<?php echo base_url()?>">Buscar <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="<?php echo base_url()?>index.php/principal/carga_info">Cargar Información</a>
+      </li>
       <li class="nav-item">
         <a class="nav-link" href="#">Graficas</a>
       </li>
-      <li class="nav-item">
-          <a class="nav-link" href="<?php echo base_url()?>index.php/principal/carga_info">Cargar Información</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Reportes</a>
-      </li>    
     </ul>
-  </div>  
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Buscar">
+      <button class="btn btn-success" type="submit">Buscar</button>
+    </form>
+  </div>
 </nav>
 <br>
